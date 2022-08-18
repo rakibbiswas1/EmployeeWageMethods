17e13ee02451ca02d36ef81951770b2e5fd123e5
rakibbiswas1 <rakibbiswas.official@gmail.com>
2022-08-19 02:51:15 +0530

    Check Employee is Present or Absent using RANDOM function

--- EmployeeWageMethod.java
+++ EmployeeWageMethod.java
@@ -2,6 +2,15 @@
 public class EmployeeWageMethod {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
+		int present = 1;
+		EmployeeWageMethod emp = new EmployeeWageMethod();
+		EmployeeWageMethod.attend(present);
 	}
-
+	public static void attend(int IS_FULL_TIME) {
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+ 		if (empCheck == IS_FULL_TIME)
+ 			System.out.println("Employee is Present");
+ 		else
+ 			System.out.println("Employee is Absent");
+	}
 }
